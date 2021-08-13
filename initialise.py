00b803349efa5f598637550d5d478456f5812c77
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-08-13 16:08:05 +1000

    Amended index format to use int ids for pages

--- initialise.py
+++ initialise.py
@@ -17,18 +17,19 @@ dbs = { 'wsdump.sqlite': '''
 				( 'https://en.wikipedia.org/wiki/Statistics' ),
 				( 'https://en.wikipedia.org/wiki/David_Hilbert' ),
 				( 'https://en.wikipedia.org/wiki/Richard_Feynman' );
-            VACUUM''',
+            VACUUM; ''',
         
         'wsindex.sqlite': '''
             DROP TABLE IF EXISTS Pages;
             CREATE TABLE Pages
-            (   url         TEXT NOT NULL PRIMARY KEY UNIQUE,
-                comp_text   TEXT,
+            (   id          INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE
+                url         TEXT NOT NULL UNIQUE,
+                zip_text    TEXT,
                 crawled     INTEGER );
             DROP TABLE IF EXISTS Links;
             CREATE TABLE Links
-            (   from_url    TEXT,
-                to_url      TEXT,
+            (   from_id     INTEGER,
+                to_id       INTEGER,
                 UNIQUE (from_url, to_url) );
             DROP TABLE IF EXISTS Words;
             CREATE TABLE Words
@@ -37,9 +38,9 @@ dbs = { 'wsdump.sqlite': '''
             DROP TABLE IF EXISTS Mentions;
             CREATE TABLE Mentions
             (   word_id     INTEGER NOT NULL,
-                url         TEXT NOT NULL,
+                page_id     TEXT NOT NULL,
                 position    INTEGER NOT NULL );
-            VACUUM'''}
+            VACUUM; '''}
 
 for f in dbs.keys():
     if os.path.isfile(f):
