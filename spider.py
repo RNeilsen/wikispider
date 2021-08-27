6e80c318bafd30d1a87b25f6a823d11f388b93c8
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-08-27 18:30:48 +1000

    Make get_more_rows more flexible

--- spider.py
+++ spider.py
@@ -14,18 +14,40 @@ wikipedia.set_rate_limiting(True)
 def get_more_rows(cur, max_to_fetch):
     max_to_fetch = min(MAX_ROWS_AT_A_TIME, max_to_fetch)
     if max_to_fetch < 1: max_to_fetch = 1
-    cur.execute(f'SELECT NULL, title FROM Open_Links ORDER BY added LIMIT {max_to_fetch}')
+    
+    # 
+    # [ ( table, ( [select_fields], order_field, [(where_req, where_val)] ) ]
+    search_order = [ 
+            # ( 'Pages',      ( ['pageid', 'title'], 'crawled', [('raw_text', None)] ) ),
+            ( 'Open_Links', ( ['NULL', 'title'], 'added', [] ) ),
+            ( 'Pages',      ( ['pageid', 'title'], 'crawled', [] ) )
+    ]
+
     rows = []
-    more_rows = cur.fetchall()
-    if more_rows is None:
-        print("Warning: no rows found in Open_Links table")
-    else:
-        rows += more_rows
-    if len(rows) < max_to_fetch:
-        cur.execute(f'SELECT page_id, title FROM Pages ORDER BY crawled LIMIT {max_to_fetch - len(rows)}')
-        more_rows = cur.fetchall()
-        if more_rows is not None:
-            rows += more_rows
+    for (table, (retrieve_fields, order_field, where_clauses)) in search_order:
+        stmt = f"SELECT {', '.join(retrieve_fields)} FROM {table} "
+        
+        where_values = tuple()
+        if where_clauses is not None and len(where_clauses) > 0:
+            stmt += 'WHERE 1 '
+            for (where_col, where_val) in where_clauses:
+                if where_val is None:
+                    stmt += 'AND ' + where_col + ' IS NULL '
+                else:
+                    stmt += 'AND ' + where_col + '=? '
+                    where_values += (where_val,)
+        
+        if order_field is not None:
+            stmt += f'ORDER BY {order_field} '
+
+        stmt += f'LIMIT {max_to_fetch - len(rows)}'
+        print("Executing:", stmt, where_values)
+        cur.execute(stmt, where_values)
+        rows += cur.fetchall()
+
+        if len(rows) >= max_to_fetch:
+            return rows
+
     if len(rows) == 0:
         raise Exception("No rows to fetch!")
     return rows
@@ -49,7 +71,6 @@ while crawled < num_to_crawl:
 
     if crawled % COMMIT_FREQ == 0:
         conn.commit()
-    crawled += 1
     
     if len(rows) == 0:
         conn.commit()
@@ -80,6 +101,8 @@ while crawled < num_to_crawl:
             continue
     print('success!', wp, flush=True)
     pageid = wp.pageid
+    
+    crawled += 1
 
     # Resolve all links to this title in Open_Links
     cur.execute('''SELECT from_id FROM Open_Links WHERE title=? OR title=?''',
@@ -92,21 +115,21 @@ while crawled < num_to_crawl:
             [(from_id[0], pageid) for from_id in from_ids if from_id is not None] )
 
     # Check if we've already crawled this page recently, skip if so
-    cur.execute('''SELECT crawled FROM Pages WHERE page_id=?''', (wp.pageid,))
+    cur.execute('''SELECT crawled FROM Pages WHERE pageid=?''', (wp.pageid,))
     found_record = cur.fetchone()
     if found_record is not None:
-        if found_record[0] >= crawl_time - RECRAWL_TIME:
+        if found_record[0] is not None and found_record[0] >= crawl_time - RECRAWL_TIME:
             continue
 
     # Enter page into Pages
     cur.execute('''INSERT OR REPLACE INTO Pages 
-            (page_id, title, raw_text, crawled) VALUES (?, ?, ?, ?)''',
+            (pageid, title, raw_text, crawled) VALUES (?, ?, ?, ?)''',
             (pageid, wp.title, wp.content, crawl_time) )
 
     # Add all of this article's links into Links (if already crawled) or Open_Links (if not)
     links = wp.links
     for link in links:
-        cur.execute('''SELECT page_id FROM Pages WHERE title=?''', (link,))
+        cur.execute('''SELECT pageid FROM Pages WHERE title=?''', (link,))
         found_link = cur.fetchone()
         if found_link is not None:
             cur.execute('''INSERT OR IGNORE INTO Links (from_id, to_id) VALUES (?,?)''', 
