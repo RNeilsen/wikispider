f402141932f3ed74aa31c5aab41246cbcf5041d1
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-09-14 14:13:20 +1000

    Add exclusive isolation level

--- vacuum.py
+++ vacuum.py
@@ -2,8 +2,14 @@ import sqlite3
 
 from initialise import INDEX_FILE_PATH
 
-conn = sqlite3.connect(INDEX_FILE_PATH)
+conn = sqlite3.connect(INDEX_FILE_PATH, isolation_level='EXCLUSIVE')
 cur = conn.cursor()
+
+print('WARNING: Ensure no spiders/indexers are running on database!')
+cont = input('Continue? (Y/n)')
+if not (cont == '' or cont.lower() == 'y'):
+    print('Aborting...')
+    exit()
 
 print('Vacuuming...', end='', flush=True)
 cur.execute('VACUUM')
